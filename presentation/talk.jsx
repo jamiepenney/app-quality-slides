a16904c90c47bcb510c3114d03f60869ec1403ac
Jamie Penney <jamie@jamiepenney.co.,nz>
2015-07-23 23:16:13 +1200

    More slides Make main titles CAPS

--- presentation/talk.jsx
+++ presentation/talk.jsx
@@ -24,7 +24,7 @@ export default class extends React.Component {
       <Deck transition={[]}>
         <Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0">
           <Image src={images.icon.replace('/','')} height="250px"/>
-          <Heading size={1}>
+          <Heading size={1} caps>
             Improving Your App Quality
           </Heading>
           <Appear>
@@ -34,7 +34,7 @@ export default class extends React.Component {
           </Appear>
         </Slide>
         <Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0">
-            <Heading size={1}>
+            <Heading size={1} caps>
             Jamie Penney
             </Heading>
             
@@ -43,7 +43,7 @@ export default class extends React.Component {
             <Link href="https://twitter.com/raygunio"><Text>Follow @raygunio on Twitter</Text></Link>
         </Slide>
         <Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start">
-            <Heading size={1}>
+            <Heading size={1} caps>
                 Ostriches
             </Heading>
         </Slide>
@@ -55,13 +55,13 @@ export default class extends React.Component {
         </Slide>
         <Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
             notes="Deploying to production can feel like being chased by a herd of Ostriches. I didn't ask for this.">
-            <Heading size={1}>
+            <Heading size={1} caps>
                 How did we end up here?
             </Heading>
         </Slide>
         <Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
             notes="It doesn't have to be like this. There is a better way">
-            <Heading size={1}>
+            <Heading size={1} caps>
                 Monitoring
             </Heading>
             <Heading size={4} italic>
@@ -76,6 +76,65 @@ export default class extends React.Component {
                 <Cite>Donald Rumsfeld</Cite>
             </BlockQuote>
         </Slide>
+        <Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start">
+            <Heading size={1} caps>
+                My daily slog
+            </Heading>
+			<Appear>
+				<List>
+					<ListItem>Read the daily digest</ListItem>
+					<ListItem>Check the support queue</ListItem>
+					<ListItem>Read the notifications</ListItem>
+					<ListItem>Check the support queue</ListItem>
+					<ListItem>Repeat until death</ListItem>
+				</List>
+			</Appear>
+        </Slide>
+		<Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
+			notes="Shows the errors that have happened in the last 24 hours">
+            <Heading size={1} caps>
+                The Daily Digest
+            </Heading>
+        </Slide>
+		<Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
+			notes="Customers seem surprised and happy when I say we've seen their error and assigned it already. I'M USING THE SAME TOOL AS YOU GUYS!">
+            <Heading size={1} caps>
+                Support
+            </Heading>
+			<Heading size={4}>
+				Probably the easiest way to gain a customer for life
+			</Heading>
+        </Slide>
+		<Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
+			notes="We use Slack and email - Email is probably redundant but I can split Apps across notification systems">
+            <Heading size={1} caps>
+                Notifications
+            </Heading>
+        </Slide>
+		<Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
+			notes="">
+            <Heading size={1} caps>
+                Handy tools
+            </Heading>
+		</Slide>
+		<Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
+			notes="This is how I know which users are experiencing errors">
+            <Heading size={1} caps>
+                User tracking
+            </Heading>
+		</Slide>
+		<Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
+			notes="URL and stack trace help me triage and assign errors">
+            <Heading size={1} caps>
+                Error details
+            </Heading>
+		</Slide>
+		<Slide bgColor="primary" bgImage={images.background.replace('/','')} bgDarken="0" align="center flex-start"
+			notes="We use Slack internally - different apps report to different channels">
+            <Heading size={1} caps>
+                Integrations
+            </Heading>
+		</Slide>
       </Deck>
     )
   }
